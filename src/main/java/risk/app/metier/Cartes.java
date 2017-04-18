158b151fb08adf8fe959c17402d80ba00ac08d20
admin <admin@192.168.10.108>
2017-04-18 12:08:20 +0200

    création des cartes

---
+++ src/main/java/risk/app/metier/Cartes.java
@@ -0,0 +1,58 @@
+package risk.app.metier;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cartes {
+
+	private Map<String, String> joueurs = new HashMap<String, String>();
+
+	public Cartes() {
+
+		this.joueurs.put("Afrique du Nord", "fantassin");
+		this.joueurs.put("Afrique du Sud", "canon");
+		this.joueurs.put("Afrique Orientale", "canon");
+		this.joueurs.put("Congo", "cavalier");
+		this.joueurs.put("Egypte", "fantassin");
+		this.joueurs.put("Madagascar", "fantassin");
+		this.joueurs.put("Alaska", "fantassin");
+		this.joueurs.put("Alberta", "fantassin");
+		this.joueurs.put("Amérique Centrale", "cavalier");
+		this.joueurs.put("Etat de l'Est", "canon");
+		this.joueurs.put("Etat de l'Ouest", "fantassin");
+		this.joueurs.put("Groenland", "cavalier");
+		this.joueurs.put("Ontario", "cavalier");
+		this.joueurs.put("Québec", "canon");
+		this.joueurs.put("Territoire du Nord Ouest", "canon");
+		this.joueurs.put("Argentine", "fantassin");
+		this.joueurs.put("Brésil", "canon");
+		this.joueurs.put("Pérou", "cavalier");
+		this.joueurs.put("Venezuela", "canon");
+		this.joueurs.put("Afghanistan", "fantassin");
+		this.joueurs.put("Chine", "cavalier");
+		this.joueurs.put("Inde", "fantassin");
+		this.joueurs.put("Japon", "fantassin");
+		this.joueurs.put("Kamchatka", "cavalier");
+		this.joueurs.put("Mongolie", "canon");
+		this.joueurs.put("Moyen Orient", "canon");
+		this.joueurs.put("Oural", "cavalier");
+		this.joueurs.put("Siam", "canon");
+		this.joueurs.put("Sibérie", "canon");
+		this.joueurs.put("Tchita", "fantassin");
+		this.joueurs.put("Yakoutie", "cavalier");
+		this.joueurs.put("Europe du Nord", "cavalier");
+		this.joueurs.put("Europe du Sud", "cavalier");
+		this.joueurs.put("Europe Occidentale", "fantassin");
+		this.joueurs.put("Grande Bretagne", "cavalier");
+		this.joueurs.put("Islande", "fantassin");
+		this.joueurs.put("Scandinavie", "canon");
+		this.joueurs.put("Ukraine", "canon");
+		this.joueurs.put("Australie Occidentale", "canon");
+		this.joueurs.put("Australie orientale", "fantassin");
+		this.joueurs.put("Indonésie", "cavalier");
+		this.joueurs.put("Nouvelle Guinée", "cavalier");
+		this.joueurs.put("Joker1", "Joker");
+		this.joueurs.put("Joker2", "Joker");
+	}
+
+}
