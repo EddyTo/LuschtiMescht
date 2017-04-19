312b9e9c045f25bcb27d1f12865c6e6374f57af3
thiba <thiba@192.168.10.110>
2017-04-19 15:03:20 +0200

    change on persistence / jUNIT test working fine now

--- src/test/java/risk/app/TestDao.java
+++ src/test/java/risk/app/TestDao.java
@@ -1,7 +1,28 @@
 package risk.app;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
 
-public class TestDao extends TestCase {
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import dao.UserDAO;
+import risk.app.model.UserList;
+
+
+public class TestDao{
+	private static UserDAO dao=null;
+	@BeforeClass
+	public static void SetUpBeforeClass() throws Exception{
+		dao=UserDAO.getInstance();
+	}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception{}
+	@Test
+	public void testEmptyBase(){
+			UserList listEmptyTable=UserDAO.findAllUsers();
+			assertEquals(0,listEmptyTable.size());
+		
+	}
 
 }
