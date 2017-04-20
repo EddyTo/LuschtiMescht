ab329435bfb575f91e456dda00871ee06acdb904
MickaelWillinger <mickael.willinger@gmail.com>
2017-04-20 16:14:52 +0200

    Combat (menage dans les lignes "code mort")

--- src/main/java/risk/app/metier/Combat.java
+++ src/main/java/risk/app/metier/Combat.java
@@ -1,7 +1,5 @@
 package risk.app.metier;
 
-
-
 public class Combat {
 
 	private static int nbDesDeffenseur1;
@@ -9,27 +7,13 @@ public class Combat {
 	private static int nbDesAttaquant1;
 	private static int nbDesAttaquant2;
 	private static int nbDesAttaquant3;
+	
 	private static Boolean desDef1 = null;
 	private static Boolean desDef2 = null;
 	private static Boolean desAtt1 = null;
 	private static Boolean desAtt2 = null;
 	private static Boolean desAtt3 = null;
 
-	static int winDef1 = 0;
-	static Boolean winDef2 = null;
-	static Boolean winDef3 = null;
-	static Boolean winDef4 = null;
-	static Boolean winDef5 = null;
-	static Boolean winDef6 = null;
-
-	static int resultatCombat1 = 0;
-	static int resultatCombat2 = 0;
-	static int resultatCombat3 = 0;
-	static int resultatCombat4 = 0;
-	static int resultatCombat5 = 0;
-	static int resultatCombat6 = 0;
-	static Boolean combatActionDeff = null;
-	static Boolean combatActionAttaque = null;
 	static int result = 0;
 
 	public static int globalFight(int desAttaquant, int desDeffenseur) {
@@ -72,12 +56,6 @@ public class Combat {
 			desDef2 = true;
 		}
 
-		resultatCombat1 = Combat.combatType1();
-		resultatCombat2 = Combat.combatType2();
-		resultatCombat3 = Combat.combatType3();
-		resultatCombat4 = Combat.combatType4();
-		resultatCombat5 = Combat.combatType5();
-		resultatCombat6 = Combat.combatType6();
 
 		return result;
 
